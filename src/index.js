108dcc399f160ef840feaa2c7742b182f0728d28
Yatin darmwal <yatindarmwal@Yatins-MacBook-Air.local>
2020-07-26 15:42:16 +0530

    filter and sort working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,107 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Table } from 'antd';
+import reqwest from 'reqwest';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const columns = [
+  {
+    title: 'Task',
+    dataIndex: 'task',
+    width: '20%',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    filters: [
+      { text: 'Pending', value: 'Pending' },
+      { text: 'Complete', value: 'Complete' },
+    ],
+    width: '20%'
+  },
+  {
+    title: 'Due Date',
+    dataIndex: 'due_date',
+    render: due_date => `${new Date(due_date * 1000)}`,
+    sorter: true
+  },
+];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const getRandomuserParams = params => {
+  return {
+    results: params.pagination.pageSize,
+    page: params.pagination.current,
+    ...params,
+  };
+};
+
+class App extends React.Component {
+  state = {
+    data: [],
+    pagination: {
+      current: 1,
+      pageSize: 10,
+    },
+    loading: false,
+  };
+
+  componentDidMount() {
+    const { pagination } = this.state;
+    this.fetch({ pagination });
+  }
+
+  handleTableChange = (pagination, filters, sorter) => {
+    console.log(sorter);
+    this.fetch({
+      sortField: sorter.field,
+      sortOrder: sorter.order,
+      pagination,
+      ...filters,
+    });
+  };
+
+  fetch = (params = {}) => {
+    this.setState({ loading: true });
+    reqwest({
+      url: '/task',
+      method: 'get',
+      type: 'json',
+      // data: getRandomuserParams(params),
+      data: {
+        user_id: '5f1d0e62f9d77c2f21d22e45',
+        filter: JSON.stringify(params.status),
+        sort_by: params.sortField,
+        sort_order: params.sortOrder
+      }
+    }).then(data => {
+      console.log(data);
+      this.setState({
+        loading: false,
+        data: data,
+        pagination: {
+          ...params.pagination,
+          total: 20,
+          // 200 is mock data, you should read it from server
+          // total: data.totalCount,
+        },
+      });
+    });
+  };
+
+  render() {
+    const { data, pagination, loading } = this.state;
+    return (
+      <Table
+        columns={columns}
+        rowKey={record => data._id}
+        dataSource={data}
+        pagination={pagination}
+        loading={loading}
+        onChange={this.handleTableChange}
+      />
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
